a0b3d951d2036fd1916277a734b8ffd8a43660f9
Paris Qian Sen <qiansen1386@gmail.com>
2016-04-08 14:14:40 +0800

    Change Line ending to Linux format

--- provision.sh
+++ provision.sh
@@ -2,10 +2,10 @@
   echo "Running Vagrant Provisioning..."
   # Create symlink to /tmp/node_modules instead of saving it in project folder right away
   PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-  echo "Detected current Project folder is "$PROJECT_DIR
-  mkdir /tmp/node_modules
+  echo -e "Detected current Project folder is $PROJECT_DIR/"
+  mkdir '/tmp/node_modules'
   chown -R vagrant:vagrant /tmp/node_modules
-  ln -s /tmp/node_modules $PROJECT_DIR/node_modules
+  ln -s '/tmp/node_modules' "$PROJECT_DIR/node_modules"
   echo "Updating Box..."
   sudo apt-get update --fix-missing # make sure the box is fully up to date
   # uncomment the following line to allow the system upgrade
@@ -17,8 +17,9 @@
   source ~/.profile # Refresh PATH
   export NVM_DIR="$HOME/.nvm"
   echo "Install & use Node 4.4.2 LTS"
-  nvm install 4.4.2 --version
-  nvm use 4.4.2
+  NODEVERSION=4.4.2
+  nvm install $NODEVERSION --version
+  nvm use $NODEVERSION
   echo "Update npm from 1.*.*/2.*.* to ^3.8.5"
   npm update -g npm
   # Install npm packages
